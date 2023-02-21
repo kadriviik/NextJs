77cad17b536c86f69b80a6572d258addb3af4258
Kadri Viik <kadri.viik@gmail.com>
2023-02-21 17:21:56 +0200

    Pre-rendering and Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData,
+        },
+    };
+}
+export default function Home ({ allPostsData }) {
   return (
     <Layout home>
         <Head>
@@ -11,6 +21,21 @@ export default function Home() {
             <p>Tere, mina olen Kadri Viik. Õpin Viljandi Kutseõppekeskuses noorem tarkvaraarendajaks. </p>
             <p> (This is a sample website - you’ll be building a site like this on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)</p>
         </section>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+            <h2 className={utilStyles.headingLg}>Blog</h2>
+            <ul className={utilStyles.list}>
+                {allPostsData.map(({ id, date, title }) => (
+                    <li className={utilStyles.listItem} key={id}>
+                        {title}
+                        <br />
+                        {id}
+                        <br />
+                        {date}
+                    </li>
+                ))}
+            </ul>
+        </section>
     </Layout>
-    );
+  );
 }
+
