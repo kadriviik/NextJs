0be6c266a00d165ed731ec904db54f674fa65e22
Kadri Viik <kadri.viik@gmail.com>
2023-02-21 16:34:22 +0200

    Assets, Metadata, and CSS

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
             <title>{siteTitle}</title>
         </Head>
         <section className={utilStyles.headingMd}>
-            <p>[Tere, mina olen Kadri Viik]</p>
+            <p>Tere, mina olen Kadri Viik. Õpin Viljandi Kutseõppekeskuses noorem tarkvaraarendajaks. </p>
             <p> (This is a sample website - you’ll be building a site like this on <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)</p>
         </section>
     </Layout>
